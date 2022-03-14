900834e9474d149a77da33522f61e1f7175fb0a8
Candice Manganar <candice.manganar@gmail.com>
2022-03-14 18:00:37 +0800

    Update checkerrors.bat

--- checkerrors.bat
+++ checkerrors.bat
@@ -1,5 +1,5 @@
 @echo off
-findstr /R /C:"exit code [^0]" /var/jenkins_home/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/log
+findstr /R /C:"exited with code [^0]" /var/jenkins_home/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/log
 
 
 if %ERRORLEVEL%==0 (
