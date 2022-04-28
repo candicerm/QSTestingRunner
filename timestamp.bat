31a1398c75afe6906facab3a2d8194dd4d8a383b
Candice Manganar <candice.manganar@gmail.com>
2022-04-28 16:13:16 +0800

    Update timestamp.bat

--- timestamp.bat
+++ timestamp.bat
@@ -4,6 +4,7 @@
 :: Returns the timestamp in yyyyMMddHHmm format
 
 @echo off
+tzutil /s "Eastern Standard Time"
 For /f "tokens=1-3 delims=:/" %%a in ("%DATE%") do (set mydate=%%c%%a%%b)
 For /f "tokens=1-2 delims=:" %%a in ("%TIME: =0%") do (set mytime=%%a%%b)
 echo %mydate%%mytime%
